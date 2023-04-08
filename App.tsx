fdf8c066a9607e5c80b8674646a196004e6c5b80
pablo salinas <79252184+PabloSalinas27@users.noreply.github.com>
2023-04-08 18:37:19 +0200

    Commit 01: “Primeros pasos en React Native”

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on amaBici!</Text>
+      <Text style={{color: "blue"}}>Open up App.tsx to start working on amabici!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0f0',
     alignItems: 'center',
     justifyContent: 'center',
   },
